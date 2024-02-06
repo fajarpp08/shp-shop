b70015a2728a7e52bb07409f92045f748a4a3bfe
unknown <fajarpp08@gmail.com>
2024-02-07 00:33:11 +0700

    add flowbite and admin dashboard

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+
+        <!-- Navbar -->
+        <Navbar />
+        <!-- Navbar End -->
+
+
+        <!-- Sidebar -->
+        <Sidebar />
+        <!-- Sidebar End-->
+        <main class="p-4 md:ml-64 h-auto pt-20">
+
+            <slot />
+            <!-- slot guna nya sama seperti yield di blade.php -->
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Navbar from '../Components/Navbar.vue'
+import Sidebar from '../Components/Sidebar.vue'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
